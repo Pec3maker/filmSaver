22a16014503773e2e0646b7ee0181fb6798fbba2
Artem <apupro5@gmail.com>
2021-11-19 13:30:59 +0300

    Removed unnecessary json names

--- app/src/main/java/com/example/inostudioTask/data/remote/dto/FilmList.kt
+++ app/src/main/java/com/example/inostudioTask/data/remote/dto/FilmList.kt
@@ -5,8 +5,8 @@ import com.squareup.moshi.Json
 
 
 data class FilmList(
-    @Json(name = "page") val page: Int?,
-    @Json(name = "results") val results: List<FilmResult>,
+    val page: Int?,
+    val results: List<FilmResult>,
     @Json(name = "total_pages") val totalPages: Int?,
     @Json(name = "total_results") val totalResults: Int?
 )
