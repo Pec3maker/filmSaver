1941d0bf623bd7b3a13035c998fdaf633ea2a083
Artem <apupro5@gmail.com>
2022-01-27 16:48:17 +0300

    refactor

--- app/src/main/java/com/example/inostudioTask/presentation/common/Screens.kt
+++ app/src/main/java/com/example/inostudioTask/presentation/common/Screens.kt
@@ -1,14 +1,5 @@
 package com.example.inostudioTask.presentation.common
 
-//enum class Screens(val route: String, val text: String) {
-//    FILM_LIST("film_list_screen", "Films"),
-//    CAST_LIST("cast_list_screen", "Cast"),
-//    FAVORITES("favorites_list_screen", "Favorites"),
-//    FILMS_REVIEWS("film_review_list_screen", "Reviews"),
-//    ACTOR_REVIEW("actor_review_screen", "Actor review"),
-//    FILM_REVIEW("film_review_screen", "Film review")
-//}
-
 sealed class Screens(val route: String, val text: String) {
     object FilmsListScreen: Screens("film_list_screen", "Films")
     object FilmReviewScreen: Screens("film_review_screen", "Film review")
